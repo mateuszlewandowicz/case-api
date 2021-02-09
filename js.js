4942a80756c791780a983ab2125eca8978a6c676
mateuszlewandowicz <lewandowicz.mateusz@outlook.com>
2021-02-09 18:53:16 +0100

    my case api

---
+++ js.js
@@ -0,0 +1,13 @@
+const btn = document.querySelector('button');
+const img = document.querySelector('img');
+const url = 'https://dog.ceo/api/breeds/image/random';
+
+btn.addEventListener('click', function(){
+    fetch(url)
+    .then(res => res.json()).then(res => {img.setAttribute('src', res.message), img.style.width ='300px'})
+    .catch(err => console.log(err))
+})
+
+
+
+
